c03222dbfd99cc3796d600fba34c57c80b373757
Anna-Bohun-art <63401934+Anna-Bohun-art@users.noreply.github.com>
2024-01-18 14:35:17 +0100

    my first commit

---
+++ about.php
@@ -0,0 +1,6 @@
+<?php include 'inc/header.php';
+?>
+    <h2>About</h2>
+
+    <p class="text-center">You are welcome here to leave your comment about our services that we have provided personally to you or your company. Please do not hesitate to leave a comment about working with us.</p>
+<?php include 'inc/footer.php';
